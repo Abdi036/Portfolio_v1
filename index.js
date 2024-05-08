a537a9609c7570e39520fc3e4ce82754f3ff5649
Abdi Kumela <abdikumela036@gmail.com>
2024-05-08 09:04:10 +0300

    selecting all the important classes

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const tabTitles = document.querySelector(".tab-titles");
+const tabLinks = document.querySelectorAll(".tab-links");
+const tabContents = document.querySelectorAll(".tab-contents");
+const openBar = document.querySelector(".fa-bars");
+const sideBar = document.querySelector(".side-bar");
+const closeBar = document.querySelector(".fa-circle-xmark");
+const navContainer = document.querySelector(".nav-container");
+
